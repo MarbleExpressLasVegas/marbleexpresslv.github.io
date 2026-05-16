5e81be67f05bd85d1b06d3ee78832e705b9a0a1b
Marble Express <rita@marbleexpresslv.com>
2026-05-15 17:17:59 -0700

    Fix

---
+++ fix.py
@@ -0,0 +1,4 @@
+﻿c = open('Container_Dashboard.html','r',encoding='utf-8').read()
+c = c.replace('<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js">function checkPW', '<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js"></script><script>function checkPW')
+open('Container_Dashboard.html','w',encoding='utf-8').write(c)
+print('Fixed')
